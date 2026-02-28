a5756bfa8ec9f66669bb8ed3b8e01503d2661b84
rajan13bhattarai-a11 <rajan13bhattarai@gmail.com>
2026-02-28 12:20:02 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,21 @@
-const CACHE = "hourcalc-pro-v1";
+const CACHE="hourcalc-pro-v2";
 
-const ASSETS = [
-  "./apple.html",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+const ASSETS=[
+"./apple.html",
+"./app.js",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener("install",e=>{
+ e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll(ASSETS))
+ );
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+ );
 });
