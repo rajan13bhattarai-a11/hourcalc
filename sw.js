01c4ef4a254fa44c78e18cdb665216d20b763139
rajan13bhattarai-a11 <rajan13bhattarai@gmail.com>
2026-02-28 12:25:10 +1100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,25 @@
-const CACHE="hourcalc-pro-v2";
+const CACHE = "hourcalc-pro-v1";
 
-const ASSETS=[
-"./apple.html",
-"./app.js",
-"./manifest.json",
-"./icon-192.png",
-"./icon-512.png"
+const ASSETS = [
+  "./apple.html",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install",e=>{
- e.waitUntil(
-  caches.open(CACHE).then(c=>c.addAll(ASSETS))
- );
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch",e=>{
- e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request))
- );
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
